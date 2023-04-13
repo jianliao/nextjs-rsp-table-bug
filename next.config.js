c189eccaf8ddd20a5340cdd46b176294ec32dac6
Jian Liao <jianliao@adobe.com>
2023-04-13 11:32:17 -0700

    disable reactStrictMode from nextjs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // RSP@v3 does not support React strict mode yet.
+  reactStrictMode: false,
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
